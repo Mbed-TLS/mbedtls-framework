a6b0b185a4b3ae5dc493f86aab53910a93ec544d
Valerio Setti <vsetti@baylibre.com>
2024-04-29 16:43:38 +0200

    run_test: add a short description for the script and license info
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- psasim/test/run_test.sh
+++ psasim/test/run_test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This is a simple bash script that tests psa_client/psa_server interaction.
+# This script is automatically executed when "make run" is launched by the
+# "psasim" root folder. The script can also be launched manually once
+# binary files are built (i.e. after "make test" is executed from the "psasim"
+# root folder).
+#
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
 set -e
 
 function clean_run() {
