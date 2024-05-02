d865a5b4d530e52e50abdb4b08b1c926882c9d8b
Valerio Setti <vsetti@baylibre.com>
2024-05-02 06:12:23 +0200

    test: wait for server startup before starting clients
    It could happen from time to time that "make run" failed due to
    the fact that the client was not able to connect to the server.
    This commit adds a simple mechanism to check that the server
    is actually ready before starting the client.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- psasim/test/run_test.sh
+++ psasim/test/run_test.sh
@@ -17,9 +17,18 @@ function clean_run() {
     ipcs | grep q | awk '{ printf " -q " $$2 }' | xargs ipcrm > /dev/null 2>&1 || true
 }
 
+# The server creates some local files when it starts up so we can wait for this
+# event as signal that the server is ready so that we can start client(s).
+function wait_for_server_startup() {
+    while [ ! -f ./psa_notify_* ]; do
+    sleep 0.1
+    done
+}
+
 clean_run
 
 ./psa_partition -k &
 SERV_PID=$!
+wait_for_server_startup
 ./psa_client
 wait $SERV_PID
