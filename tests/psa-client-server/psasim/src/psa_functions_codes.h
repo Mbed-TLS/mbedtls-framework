aac7f0bcbbe27aba2777d11f39f4085b9d7bdff5
David Horstmann <david.horstmann@arm.com>
2024-05-29 16:01:43 +0100

    Move some files to framework repository
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- tests/psa-client-server/psasim/src/psa_functions_codes.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PSA_FUNCTIONS_CODES_H_
-#define  _PSA_FUNCTIONS_CODES_H_
-
-enum {
-    PSA_CRYPTO_INIT = 0x00,
-    /* Add other PSA functions here */
-};
-
-#endif /*  _PSA_FUNCTIONS_CODES_H_ */
