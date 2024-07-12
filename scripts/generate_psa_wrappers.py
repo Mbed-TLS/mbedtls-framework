9819b048a690966a891bdf005772e19e5c90bb57
David Horstmann <david.horstmann@arm.com>
2024-07-12 18:58:22 +0100

    Update references to tests/src
    Update paths in generating scripts that refer to tests/src. These will
    now generate files instead in framework/tests/src (relative to the
    main repo root directory).
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- scripts/generate_psa_wrappers.py
+++ scripts/generate_psa_wrappers.py
@@ -248,8 +248,8 @@ class PSALoggingWrapperGenerator(PSAWrapperGenerator, c_wrapper_generator.Loggin
         return super()._printf_parameters(typ, var)
 
 
-DEFAULT_C_OUTPUT_FILE_NAME = 'tests/src/psa_test_wrappers.c'
-DEFAULT_H_OUTPUT_FILE_NAME = 'tests/include/test/psa_test_wrappers.h'
+DEFAULT_C_OUTPUT_FILE_NAME = 'framework/tests/src/psa_test_wrappers.c'
+DEFAULT_H_OUTPUT_FILE_NAME = 'framework/tests/include/test/psa_test_wrappers.h'
 
 def main() -> None:
     parser = argparse.ArgumentParser(description=globals()['__doc__'])
