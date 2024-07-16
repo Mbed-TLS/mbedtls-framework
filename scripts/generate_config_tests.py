a758beb103e7615e2ed0abbbdb3b0a5edb6ad695
Gabor Mezei <gabor.mezei@arm.com>
2024-07-16 12:48:09 +0200

    Add compatibility with 3.6
    Use the old `ConfigFile` class for the 3.6
    
    Signed-off-by: Gabor Mezei <gabor.mezei@arm.com>

--- scripts/generate_config_tests.py
+++ scripts/generate_config_tests.py
@@ -5,6 +5,7 @@
 # Copyright The Mbed TLS Contributors
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
 
+import abc
 import re
 import sys
 from typing import Iterable, Iterator, List, Optional, Tuple
@@ -158,10 +159,13 @@ class ConfigTestGenerator(test_data_generation.TestGenerator):
     """Generate test cases for configuration reporting."""
 
     def __init__(self, settings):
-        self.mbedtls_config = config.MbedTLSConfig()
+        # Temporarily use different config classes for 3.6. With the config.py moving to
+        # the framework it will be unified.
+        is_3_6 = not isinstance(config.ConfigFile, abc.ABCMeta)
+        self.mbedtls_config = config.ConfigFile() if is_3_6 else config.MbedTLSConfig()
         self.targets['test_suite_config.mbedtls_boolean'] = \
             lambda: enumerate_boolean_setting_cases(self.mbedtls_config)
-        self.psa_config = config.CryptoConfig()
+        self.psa_config = config.ConfigFile('include/psa/crypto_config.h') if is_3_6 else config.CryptoConfig()
         self.targets['test_suite_config.psa_boolean'] = \
             lambda: enumerate_boolean_setting_cases(self.psa_config)
         super().__init__(settings)
