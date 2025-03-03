3c889a4f3a85ca47ed1a89d8fb953bcdca92450d
Gilles Peskine <Gilles.Peskine@arm.com>
2025-03-03 16:48:31 +0100

    Tweak log message assertions
    In preparation for reworking mbedtls_ssl_prepare_handshake_record(),
    don't assert negatively on "reassembled record".
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/generate_tls_handshake_tests.py
+++ scripts/generate_tls_handshake_tests.py
@@ -104,7 +104,6 @@ def write_tls_handshake_defragmentation_test(
 
     if length is None:
         forbidden_patterns = [
-            'reassembled record',
             'waiting for more fragments',
         ]
         wanted_patterns = []
