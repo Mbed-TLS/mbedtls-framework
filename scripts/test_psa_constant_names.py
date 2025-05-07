ebc32adfeee66c6c4671b547f75903f761dcfa42
Ben Taylor <ben.taylor@linaro.org>
2025-05-07 13:42:22 +0100

    add line concatination for everest headers
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- scripts/test_psa_constant_names.py
+++ scripts/test_psa_constant_names.py
@@ -173,7 +173,7 @@ def main():
                             action='append', default=['tf-psa-crypto/include',
                                                       'tf-psa-crypto/drivers/builtin/include',
                                                       'tf-psa-crypto/drivers/everest/include',
-                                                      'tf-psa-crypto/drivers/everest/include/'
+                                                      'tf-psa-crypto/drivers/everest/include/' +
                                                       'tf-psa-crypto/private',
                                                       'include'],
                             help='Directory for header files')
