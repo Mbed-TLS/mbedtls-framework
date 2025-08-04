d4da396357d694fe1c3bcf212c36175c86df8bc5
Ben Taylor <ben.taylor@linaro.org>
2025-08-04 13:35:41 +0100

    fix code style issues
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- tests/src/psa_exercise_key.c
+++ tests/src/psa_exercise_key.c
@@ -1390,7 +1390,8 @@ int mbedtls_test_key_consistency_psa_pk(mbedtls_svc_key_id_t psa_key,
                 PSA_KEY_TYPE_IS_KEY_PAIR(psa_type));
     TEST_EQUAL(psa_get_key_bits(&psa_attributes), mbedtls_pk_get_bitlen(pk));
 
-#if (MBEDTLS_VERSION_MAJOR >= 4) || (TF_PSA_CRYPTO_VERSION_MAJOR >= 1 ) || !defined(MBEDTLS_PK_USE_PSA_RSA_DATA)
+#if (MBEDTLS_VERSION_MAJOR >= 4) || (TF_PSA_CRYPTO_VERSION_MAJOR >= 1) ||  \
+    !defined(MBEDTLS_PK_USE_PSA_RSA_DATA)
     uint8_t pk_public_buffer[PSA_EXPORT_PUBLIC_KEY_MAX_SIZE];
 #endif /* (MBEDTLS_VERSION_MAJOR >= 4) || (TF_PSA_CRYPTO_VERSION_MAJOR >= 1 ) */
     const uint8_t *pk_public = NULL;
@@ -1441,7 +1442,7 @@ int mbedtls_test_key_consistency_psa_pk(mbedtls_svc_key_id_t psa_key,
 #endif /* MBEDTLS_PK_USE_PSA_EC_DATA */
 #endif /* PSA_WANT_KEY_TYPE_ECC_PUBLIC_KEY */
 
-#if (MBEDTLS_VERSION_MAJOR >= 4) || (TF_PSA_CRYPTO_VERSION_MAJOR >= 1 )
+#if (MBEDTLS_VERSION_MAJOR >= 4) || (TF_PSA_CRYPTO_VERSION_MAJOR >= 1)
         case MBEDTLS_PK_OPAQUE:
             PSA_ASSERT(psa_get_key_attributes(pk->priv_id, &pk_attributes));
             psa_key_type_t pk_psa_type = psa_get_key_type(&pk_attributes);
@@ -1453,7 +1454,7 @@ int mbedtls_test_key_consistency_psa_pk(mbedtls_svc_key_id_t psa_key,
                                              &pk_public_length));
             pk_public = pk_public_buffer;
             break;
-#endif /* (MBEDTLS_VERSION_MAJOR >= 4) || (TF_PSA_CRYPTO_VERSION_MAJOR >= 1 ) */
+#endif /* (MBEDTLS_VERSION_MAJOR >= 4) || (TF_PSA_CRYPTO_VERSION_MAJOR >= 1) */
 
         default:
             TEST_FAIL("pk type not supported");
