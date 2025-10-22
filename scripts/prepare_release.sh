c4d4f6b4a12fe758c777f6d3443dbc118a7d3f02
Minos Galanakis <minos.galanakis@arm.com>
2025-10-22 11:48:09 +0100

    prepare_release.sh: Removed -r/-u modes
    Signed-off-by: Minos Galanakis <minos.galanakis@arm.com>

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-print_usage()
-{
-    cat <<EOF
-Usage: $0 [OPTION]...
-Prepare the source tree for a release.
-
-Options:
-  -r    Prepare for release
-  -u    Prepare for development (undo the release preparation)
-EOF
-}
-
 # Copyright The Mbed TLS Contributors
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
 
@@ -29,42 +16,12 @@ psed() {
     fi
 }
 
-if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
-    print_usage
-    exit
-fi
-
-unrelease=0 # if 1 then we are in development mode,
-           # if 0 then we are in release mode
-while getopts ru OPTLET; do
-    case $OPTLET in
-        u) unrelease=1;;
-        r) unrelease=0;;
-        \?)
-            echo 1>&2 "$0: unknown option: -$OPTLET"
-            echo 1>&2 "Try '$0 --help' for more information."
-            exit 3;;
-    esac
-done
-
 #### .gitignore processing ####
 for GITIGNORE in $(git ls-files -- '*.gitignore'); do
-    if [ "$unrelease" -eq 1 ]; then
-        psed '/###START_COMMENTED_GENERATED_FILES###/,/###END_COMMENTED_GENERATED_FILES###/s/^#//' "$GITIGNORE"
-        psed 's/###START_COMMENTED_GENERATED_FILES###/###START_GENERATED_FILES###/' "$GITIGNORE"
-        psed 's/###END_COMMENTED_GENERATED_FILES###/###END_GENERATED_FILES###/' "$GITIGNORE"
-    else
         psed '/###START_GENERATED_FILES###/,/###END_GENERATED_FILES###/s/^/#/' "$GITIGNORE"
         psed 's/###START_GENERATED_FILES###/###START_COMMENTED_GENERATED_FILES###/' "$GITIGNORE"
         psed 's/###END_GENERATED_FILES###/###END_COMMENTED_GENERATED_FILES###/' "$GITIGNORE"
-    fi
 done
 
-#### Build scripts ####
-# GEN_FILES defaults on in development, off in releases
-if [ "$unrelease" -eq 1 ]; then
-    r='ON'
-else
-    r='OFF'
-fi
-psed '/[Oo][Ff][Ff] in development/! s/^\( *option *( *GEN_FILES  *"[^"]*"  *\)\([A-Za-z0-9][A-Za-z0-9]*\)/\1'"$r/" CMakeLists.txt
+#### Build system ####
+psed '/[Oo][Ff][Ff] in development/! s/^\( *option *( *GEN_FILES  *"[^"]*"  *\)\([A-Za-z0-9][A-Za-z0-9]*\)/\1'"OFF/" CMakeLists.txt
