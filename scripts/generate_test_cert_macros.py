b14c835347f199436ad10db9a842f71c0953f958
Gilles Peskine <Gilles.Peskine@arm.com>
2025-10-27 09:42:00 +0100

    Fix absolute path in generated file
    That made the build non-reproducible.
    
    Addresses https://github.com/Mbed-TLS/mbedtls/issues/9521
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/generate_test_cert_macros.py
+++ scripts/generate_test_cert_macros.py
@@ -16,40 +16,40 @@ import jinja2
 from mbedtls_framework.build_tree import guess_project_root
 
 TESTS_DIR = os.path.join(guess_project_root(), 'tests')
-FRAMEWORK_DIR = os.path.join(guess_project_root(), 'framework')
-DATA_FILES_PATH = os.path.join(FRAMEWORK_DIR, 'data_files')
+DATA_DIR_RELATIVE = 'framework/data_files'
+DATA_DIR_ABSOLUTE = os.path.join(guess_project_root(), DATA_DIR_RELATIVE)
 
 INPUT_ARGS = [
-    ("string", "TEST_CA_CRT_EC_PEM", DATA_FILES_PATH + "/test-ca2.crt"),
-    ("binary", "TEST_CA_CRT_EC_DER", DATA_FILES_PATH + "/test-ca2.crt.der"),
-    ("string", "TEST_CA_KEY_EC_PEM", DATA_FILES_PATH + "/test-ca2.key.enc"),
+    ("string", "TEST_CA_CRT_EC_PEM", "test-ca2.crt"),
+    ("binary", "TEST_CA_CRT_EC_DER", "test-ca2.crt.der"),
+    ("string", "TEST_CA_KEY_EC_PEM", "test-ca2.key.enc"),
     ("password", "TEST_CA_PWD_EC_PEM", "PolarSSLTest"),
-    ("binary", "TEST_CA_KEY_EC_DER", DATA_FILES_PATH + "/test-ca2.key.der"),
-    ("string", "TEST_CA_CRT_RSA_SHA256_PEM", DATA_FILES_PATH + "/test-ca-sha256.crt"),
-    ("binary", "TEST_CA_CRT_RSA_SHA256_DER", DATA_FILES_PATH + "/test-ca-sha256.crt.der"),
-    ("string", "TEST_CA_CRT_RSA_SHA1_PEM", DATA_FILES_PATH + "/test-ca-sha1.crt"),
-    ("binary", "TEST_CA_CRT_RSA_SHA1_DER", DATA_FILES_PATH + "/test-ca-sha1.crt.der"),
-    ("string", "TEST_CA_KEY_RSA_PEM", DATA_FILES_PATH + "/test-ca.key"),
+    ("binary", "TEST_CA_KEY_EC_DER", "test-ca2.key.der"),
+    ("string", "TEST_CA_CRT_RSA_SHA256_PEM", "test-ca-sha256.crt"),
+    ("binary", "TEST_CA_CRT_RSA_SHA256_DER", "test-ca-sha256.crt.der"),
+    ("string", "TEST_CA_CRT_RSA_SHA1_PEM", "test-ca-sha1.crt"),
+    ("binary", "TEST_CA_CRT_RSA_SHA1_DER", "test-ca-sha1.crt.der"),
+    ("string", "TEST_CA_KEY_RSA_PEM", "test-ca.key"),
     ("password", "TEST_CA_PWD_RSA_PEM", "PolarSSLTest"),
-    ("binary", "TEST_CA_KEY_RSA_DER", DATA_FILES_PATH + "/test-ca.key.der"),
-    ("string", "TEST_SRV_CRT_EC_PEM", DATA_FILES_PATH + "/server5.crt"),
-    ("binary", "TEST_SRV_CRT_EC_DER", DATA_FILES_PATH + "/server5.crt.der"),
-    ("string", "TEST_SRV_KEY_EC_PEM", DATA_FILES_PATH + "/server5.key"),
-    ("binary", "TEST_SRV_KEY_EC_DER", DATA_FILES_PATH + "/server5.key.der"),
-    ("string", "TEST_SRV_CRT_RSA_SHA256_PEM", DATA_FILES_PATH + "/server2-sha256.crt"),
-    ("binary", "TEST_SRV_CRT_RSA_SHA256_DER", DATA_FILES_PATH + "/server2-sha256.crt.der"),
-    ("string", "TEST_SRV_CRT_RSA_SHA1_PEM", DATA_FILES_PATH + "/server2.crt"),
-    ("binary", "TEST_SRV_CRT_RSA_SHA1_DER", DATA_FILES_PATH + "/server2.crt.der"),
-    ("string", "TEST_SRV_KEY_RSA_PEM", DATA_FILES_PATH + "/server2.key"),
-    ("binary", "TEST_SRV_KEY_RSA_DER", DATA_FILES_PATH + "/server2.key.der"),
-    ("string", "TEST_CLI_CRT_EC_PEM", DATA_FILES_PATH + "/cli2.crt"),
-    ("binary", "TEST_CLI_CRT_EC_DER", DATA_FILES_PATH + "/cli2.crt.der"),
-    ("string", "TEST_CLI_KEY_EC_PEM", DATA_FILES_PATH + "/cli2.key"),
-    ("binary", "TEST_CLI_KEY_EC_DER", DATA_FILES_PATH + "/cli2.key.der"),
-    ("string", "TEST_CLI_CRT_RSA_PEM", DATA_FILES_PATH + "/cli-rsa-sha256.crt"),
-    ("binary", "TEST_CLI_CRT_RSA_DER", DATA_FILES_PATH + "/cli-rsa-sha256.crt.der"),
-    ("string", "TEST_CLI_KEY_RSA_PEM", DATA_FILES_PATH + "/cli-rsa.key"),
-    ("binary", "TEST_CLI_KEY_RSA_DER", DATA_FILES_PATH + "/cli-rsa.key.der"),
+    ("binary", "TEST_CA_KEY_RSA_DER", "test-ca.key.der"),
+    ("string", "TEST_SRV_CRT_EC_PEM", "server5.crt"),
+    ("binary", "TEST_SRV_CRT_EC_DER", "server5.crt.der"),
+    ("string", "TEST_SRV_KEY_EC_PEM", "server5.key"),
+    ("binary", "TEST_SRV_KEY_EC_DER", "server5.key.der"),
+    ("string", "TEST_SRV_CRT_RSA_SHA256_PEM", "server2-sha256.crt"),
+    ("binary", "TEST_SRV_CRT_RSA_SHA256_DER", "server2-sha256.crt.der"),
+    ("string", "TEST_SRV_CRT_RSA_SHA1_PEM", "server2.crt"),
+    ("binary", "TEST_SRV_CRT_RSA_SHA1_DER", "server2.crt.der"),
+    ("string", "TEST_SRV_KEY_RSA_PEM", "server2.key"),
+    ("binary", "TEST_SRV_KEY_RSA_DER", "server2.key.der"),
+    ("string", "TEST_CLI_CRT_EC_PEM", "cli2.crt"),
+    ("binary", "TEST_CLI_CRT_EC_DER", "cli2.crt.der"),
+    ("string", "TEST_CLI_KEY_EC_PEM", "cli2.key"),
+    ("binary", "TEST_CLI_KEY_EC_DER", "cli2.key.der"),
+    ("string", "TEST_CLI_CRT_RSA_PEM", "cli-rsa-sha256.crt"),
+    ("binary", "TEST_CLI_CRT_RSA_DER", "cli-rsa-sha256.crt.der"),
+    ("string", "TEST_CLI_KEY_RSA_PEM", "cli-rsa.key"),
+    ("binary", "TEST_CLI_KEY_RSA_DER", "cli-rsa.key.der"),
 ]
 
 def main():
@@ -61,7 +61,8 @@ def main():
 
     if args.list_dependencies:
         files_list = [arg[2] for arg in INPUT_ARGS]
-        print(" ".join(files_list))
+        print(" ".join(os.path.join(DATA_DIR_RELATIVE, filename)
+                       for filename in files_list))
         return
 
     generate(INPUT_ARGS, output=args.output)
@@ -70,20 +71,20 @@ def main():
 def generate(values=[], output=None):
     """Generate C header file.
     """
-    template_loader = jinja2.FileSystemLoader(DATA_FILES_PATH)
+    template_loader = jinja2.FileSystemLoader(DATA_DIR_ABSOLUTE)
     template_env = jinja2.Environment(
         loader=template_loader, lstrip_blocks=True, trim_blocks=True,
         keep_trailing_newline=True)
 
     def read_as_c_array(filename):
-        with open(filename, 'rb') as f:
+        with open(os.path.join(DATA_DIR_ABSOLUTE, filename), 'rb') as f:
             data = f.read(12)
             while data:
                 yield ', '.join(['{:#04x}'.format(b) for b in data])
                 data = f.read(12)
 
     def read_lines(filename):
-        with open(filename) as f:
+        with open(os.path.join(DATA_DIR_ABSOLUTE, filename)) as f:
             try:
                 for line in f:
                     yield line.strip()
@@ -101,7 +102,8 @@ def generate(values=[], output=None):
     template = template_env.get_template('test_certs.h.jinja2')
 
     with open(output, 'w') as f:
-        f.write(template.render(macros=values))
+        f.write(template.render(data_dir=DATA_DIR_RELATIVE,
+                                macros=values))
 
 
 if __name__ == '__main__':
