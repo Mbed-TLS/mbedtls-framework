0035cfb1f05b7a90fc786169349cc1eccc61f6f1
Ben Taylor <ben.taylor@linaro.org>
2025-10-30 13:42:30 +0000

    Removed unnecessary cast in mbedtls_pk_sign_ext
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- library/ssl_tls12_server.c
+++ library/ssl_tls12_server.c
@@ -2880,7 +2880,7 @@ curve_matching_done:
          * after the call to ssl_prepare_server_key_exchange.
          * ssl_write_server_key_exchange also takes care of incrementing
          * ssl->out_msglen. */
-        if ((ret = mbedtls_pk_sign_ext((mbedtls_pk_sigalg_t) sig_alg, mbedtls_ssl_own_key(ssl),
+        if ((ret = mbedtls_pk_sign_ext(sig_alg, mbedtls_ssl_own_key(ssl),
                                        md_alg, hash, hashlen,
                                        ssl->out_msg + ssl->out_msglen + 2,
                                        out_buf_len - ssl->out_msglen - 2,
