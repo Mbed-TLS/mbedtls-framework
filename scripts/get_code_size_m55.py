87c57e82a1eaa432a12ddefcf2fbc72d81412442
David Horstmann <david.horstmann@arm.com>
2025-12-18 15:01:53 +0000

    Parameterise size command
    This allows us to specify the platform version of size if required,
    while keeping the default as arm-none-eabi-size.
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- scripts/get_code_size_m55.py
+++ scripts/get_code_size_m55.py
@@ -7,6 +7,7 @@ existing scripts.
 # Copyright The Mbed TLS Contributors
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
 
+import argparse
 import json
 import os
 import subprocess
@@ -17,11 +18,11 @@ def build_library(build_dir, toolchain_file):
                            '-DENABLE_PROGRAMS=NO'])
     subprocess.check_call(['cmake', '--build', build_dir, '-j' + str(os.cpu_count())])
 
-def generate_sizes(build_dir):
+def generate_sizes(build_dir, size_cmd):
     subprocess.check_call(['framework/scripts/generate_code_size_report.py',
                            '--output-file', build_dir + '/code_size.json',
                            '--library-file', build_dir + '/core/libtfpsacrypto.a',
-                           '--size-cmd', 'arm-none-eabi-size'])
+                           '--size-cmd', size_cmd])
 
 def display_sizes(build_dir):
     subprocess.check_call(['framework/scripts/show_code_size.py',
@@ -30,6 +31,15 @@ def display_sizes(build_dir):
 if __name__ == '__main__':
     BUILD_DIR = 'build-code-size-m55'
 
+    parser = argparse.ArgumentParser(
+        description='Generate a code size report in JSON format')
+
+    parser.add_argument('-s', '--size-cmd',
+                        help='Size command to use (default arm-none-eabi-size)',
+                        default='arm-none-eabi-size')
+
+    args = parser.parse_args()
+
     build_library(BUILD_DIR, 'arm-gcc-m55.cmake')
-    generate_sizes(BUILD_DIR)
+    generate_sizes(BUILD_DIR, args.size_cmd)
     display_sizes(BUILD_DIR)
