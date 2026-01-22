2aecd2cd5fe0babe94fb971b6191d75c3ceacbf9
Valerio Setti <valerio.setti@nordicsemi.no>
2026-01-22 17:13:44 +0100

    library|tests: ssl: remove secp256k1 from default groups
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -2373,9 +2373,6 @@ static const uint16_t ssl_preset_default_groups[] = {
 #if defined(PSA_WANT_ECC_SECP_R1_256)
     MBEDTLS_SSL_IANA_TLS_GROUP_SECP256R1,
 #endif
-#if defined(PSA_WANT_ECC_SECP_K1_256)
-    MBEDTLS_SSL_IANA_TLS_GROUP_SECP256K1,
-#endif
 #if defined(PSA_WANT_ECC_SECP_R1_384)
     MBEDTLS_SSL_IANA_TLS_GROUP_SECP384R1,
 #endif
