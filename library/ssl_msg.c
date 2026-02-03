7fe38dd9343dca7e404bae1e13b01e3637184e79
Ronald Cron <ronald.cron@arm.com>
2026-02-03 09:58:21 +0100

    ssl_msg.c: Improve HS message reassembly completed message
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- library/ssl_msg.c
+++ library/ssl_msg.c
@@ -18,6 +18,7 @@
 
 #include "mbedtls/ssl.h"
 #include "debug_internal.h"
+#include "ssl_debug_helpers.h"
 #include "mbedtls/error.h"
 #include "mbedtls/platform_util.h"
 #include "mbedtls/version.h"
@@ -4157,7 +4158,9 @@ static int ssl_load_buffered_message(mbedtls_ssl_context *ssl)
             return MBEDTLS_ERR_SSL_INTERNAL_ERROR;
         }
 
-        MBEDTLS_SSL_DEBUG_MSG(2, ("Next handshake message has been buffered - load"));
+        MBEDTLS_SSL_DEBUG_MSG(2, ("%s handshake message has been buffered%s",
+                                  mbedtls_ssl_get_hs_msg_name(hs_buf->data[0]),
+                                  hs_buf->is_fragmented ? " and reassembled" : ""));
         MBEDTLS_SSL_DEBUG_BUF(3, "Buffered handshake message (incl. header)",
                               hs_buf->data, msg_len + 12);
 
